a51b25d52102cd4cc26f939be8f21240558a1372
bunnkei0@gmail.com <positivethinking0305@gmail.com>
2022-04-11 10:09:43 +0900

    first

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheet.css">
+    <title>harry styles</title>
+</head>
+
+<body>
+    <div class="body0">
+        <div class="header">
+            <h1>Harry Styles</h1>
+            
+        </div>
+        <h2>welcome to my homepage</h2>
+        <h3>このホームページは歌手・ハリースタイルズの写真を載せただけのページとなっております</h3>
+
+        <main>
+            <img src="./img/harry1.jpg" alt="harry">
+            <img src="./img/harry2.jpg" alt="harry">
+            <img src="./img/harry3.jpg" alt="harry">
+            <img src="./img/harry4.jpg" alt="harry">
+            <img src="./img/harry5.jpg" alt="harry">
+            <img src="./img/harry6.jpg" alt="harry">
+            <img src="./img/harry7.jpg" alt="harry">
+            <img src="./img/harry8.jpg" alt="harry">
+            <img src="./img/harry9.jpg" alt="harry">
+            <img src="./img/harry10.jpg" alt="harry">
+            <a href="https://youtu.be/H5v3kku4y6Q"><img src="./img/harry11.jpg" alt="harry"></a>←この写真をクリック
+        </main>
+        <br><br><br><br>
+        <footer>
+            
+            
+            <h2>今日、学んだこと</h2>
+            <ul>
+                <li>フォントの大きさ</li>
+                <li>画像の挿入</li>
+                <li>ページの分割</li>
+                <li>箇条書きの方法</li>
+                <li>リンクの挿入</li>
+                <li>改行</li>
+                <li>横並びにする</li>
+            </ul>
+            <br><br>
+            <h2>今日の一言</h2>
+            <p>朝、目を覚ますと暖かな光とともに鳥のさえずりが聞こえてくる。<br>
+                カーテンを開くと朝紅色の桜と紺碧の大空。<br>
+                なかなか乙な日じゃぁねぇか。<br>
+                ハリーの歌声で癒されるのもいいけど、季節を満喫するのもまた一興。また明日！！</p>
+        </footer>
+
+
+    </div>
+
+    <div class="body1">
+        <div class="header1">
+            <h1>Emma Watson</h1>
+            
+        </div>
+        <h2>welcome to my homepage</h2>
+        <h3>このホームページは女優・エマワトソンの写真を載せただけのページとなっております</h3>
+        
+        
+
+        <main>
+            <img src="./img/emma1.jpg" alt="emma">
+            <img src="./img/emma2.jpg" alt="emma">
+            <img src="./img/emma3.jpg" alt="emma">
+            <img src="./img/emma4.jpg" alt="emma">
+            <img src="./img/emma5.jpg" alt="emma">
+            <img src="./img/emma6.jpg" alt="emma">
+            <img src="./img/emma7.jpg" alt="emma">
+            <img src="./img/emma8.jpg" alt="emma">
+            <img src="./img/emma9.jpg" alt="emma">
+            <!-- <img src="./emma10.jpg" alt="emma"> -->
+            <img src="./img/emma10.jpg" alt="emma">
+        </main>
+        <br><br><br><br>
+        <footer>
+            
+            
+            <h2>今日、学んだこと</h2>
+            <ul>
+                <li>スクレーピングとは</li>
+                <li>使用上の注意点</li>
+                <li>検証の使いかた</li>
+                <li>タグの指定方法</li>
+            </ul>
+            <br><br>
+            <!-- <h2>今日の一言</h2>
+            <p>朝、目を覚ますと暖かな光とともに鳥のさえずりが聞こえてくる。<br>
+                カーテンを開くと朝紅色の桜と紺碧の大空。<br>
+                なかなか乙な日じゃぁねぇか。<br>
+                ハリーの歌声で癒されるのもいいけど、季節を満喫するのもまた一興。また明日！！</p> -->
+        </footer>
+
+    </div>
+
+
+</body>
+
+
+</html>
